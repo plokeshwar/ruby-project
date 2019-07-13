d7472540b9c6cfd505e56959041908a3170517ba
kvsivaprasannaaa <kvsivaprasanna@gmail.com>
2019-07-13 16:13:45 +0530

    added customer pages

---
+++ selenium/features/pages/common_methods.rb
@@ -0,0 +1,12 @@
+class CommonMethods
+
+      def generate_random_number
+      return rand.to_s[2..8]
+      end
+
+      def get_random_string(length=5)
+   source=("a".."z").to_a + ("A".."Z").to_a + (0..9).to_a + ["_","-","."]
+   key=""
+   length.times{ key += source[rand(source.size)].to_s }
+   return key
+ end
